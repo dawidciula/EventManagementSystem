c158023605daa829d9c85cf563b36f3d58156f0b
Artur Zelek <1a.artur.zelek@gmail.com>
2024-05-01 16:19:41 +0200

    Update Django version and add Google authentication

--- Event/app/models.py
+++ Event/app/models.py
@@ -23,6 +23,7 @@ class UserManager(BaseUserManager):
             raise ValueError("A password is required.")
         user = self.create_user(email, password)
         user.is_superuser = True
+        user.is_staff = True
         user.save()
         return user
 
@@ -31,6 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     user_ID = models.AutoField(primary_key=True)
     email = models.EmailField(max_length=50, unique=True)
     username = models.CharField(max_length=50)
+    is_staff = models.BooleanField(default=False)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
     objects = UserManager()
